420739826626227228d958b95ac4d360d3692d78
sambrsk <111059452+sambrsk@users.noreply.github.com>
2023-01-10 16:17:27 +0000

    Reverted to sensible defaults

--- Base Installation/Program.cs
+++ Base Installation/Program.cs
@@ -3,12 +3,10 @@ using IdentityExpress.Manager.UI.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddAdminUI();
-
 builder.Services.AddAdminUI(options =>
 {
-    options.IdentityType = IdentityType.DefaultIdentity;
-    options.MigrationOptions = MigrationOptions.None;
+//     options.IdentityType = IdentityType.DefaultIdentity;
+//     options.MigrationOptions = MigrationOptions.None;
 });
 
 var app = builder.Build();
