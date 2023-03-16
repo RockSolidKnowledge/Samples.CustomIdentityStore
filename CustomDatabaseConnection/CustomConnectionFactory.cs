092415fa92eeaae1412111d84eb50a9f0f039ecd
Simon Horrell <simon@rocksolidknowledge.com>
2023-03-16 07:34:41 +0000

    Added sample of using custom connection factory against Rsk.AdminUI 6.7.0-alpha.56521

---
+++ CustomDatabaseConnection/CustomConnectionFactory.cs
@@ -0,0 +1,35 @@
+﻿using IdentityExpress.Manager.UI.Extensions;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace CustomDatabaseConnection
+{
+    internal class CustomConnectionFactory : IDatabaseConnectionFactory
+    {
+        private readonly string connectionString = "Data Source=localhost\\SQLEXPRESS;User Id=AdminUI-User;Password=WoyblhD5G7gz7LA5Nz7e;Database=IdentityExpressDb;";
+        public DbConnection CreateAuditRecordsConnection(bool migration = false)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DbConnection CreateDataProtectionConnection(bool migration = false)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DbConnection CreateIdentityConnection(bool migration = false)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DbConnection CreateIdentityServerConnection(bool migration = false)
+        {
+            return new SqlConnection(connectionString);
+        }
+
+        public DbConnection CreateOperationalConnection(bool migration = false)
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ CustomDatabaseConnection/Program.cs
@@ -0,0 +1,20 @@
+﻿using CustomDatabaseConnection;
+using IdentityExpress.Manager.BusinessLogic.Configuration;
+using IdentityExpress.Manager.UI.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAdminUI(options =>
+    {
+        options.DatabaseConnectionFactoryType = DatabaseConnectionFactoryType.Custom;
+    })
+    .WithConnectionFactory<CustomConnectionFactory>();
+
+var app = builder.Build();
+
+await app.RunMigrationsAsync();
+
+app.UseAdminUI();
+
+app.Run();
