f1c5fcd1a1f23dfa99228a9ae3bc419104325799
Simon Horrell <simon@rocksolidknowledge.com>
2023-03-17 22:20:48 +0000

    Built against Rsk.AdminUI 6.7.0.56637

--- CustomDatabaseConnection/Program.cs
+++ CustomDatabaseConnection/Program.cs
@@ -1,19 +1,26 @@
 ﻿using CustomDatabaseConnection;
 using IdentityExpress.Manager.BusinessLogic.Configuration;
+using IdentityExpress.Manager.MigrationRunner;
 using IdentityExpress.Manager.UI.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddAdminUI(options =>
-    {
-        options.DatabaseConnectionFactoryType = DatabaseConnectionFactoryType.Custom;
-    })
-    .WithConnectionFactory<CustomConnectionFactory>();
+    .AddAdminUI(
+    //Uncomment this to use custom connection factory
+    //options =>
+    //{
+    //    options.DatabaseConnectionFactoryType = DatabaseConnectionFactoryType.Custom;
+    //}
+    )
+  //Uncomment this to use custom connection factory
+  //.WithConnectionFactory<CustomConnectionFactory>()
+    ;
 
 var app = builder.Build();
 
-await app.RunMigrationsAsync();
+//Uncomment this to run migrations
+//await app.RunMigrationsAsync(MigrationType.All);
 
 app.UseAdminUI();
 
